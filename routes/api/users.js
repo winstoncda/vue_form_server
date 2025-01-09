e0df456975a7d7b1ad70cae2d3eecb9ee93f7bde
winstoncda <olivier.pro.cda@gmail.com>
2025-01-09 15:48:58 +0100

    décommenter la route post

--- routes/api/users.js
+++ routes/api/users.js
@@ -14,24 +14,29 @@ router.get("/", (req, res) => {
 router.post("/register", async (req, res) => {
   console.log(req.body);
 
-  // const { username, password } = req.body;
-  // const hashedPassword = await bcrypt.hash(password, 10);
-  // const insertSql = "INSERT INTO users (username, password) VALUES (?, ?)";
-  // connection.query(insertSql, [username, hashedPassword], (err, result) => {
-  //   if (err) throw err;
-  //   let lastInsertId = result.insertId;
-  //   const token = jwt.sign(
-  //     { id: lastInsertId, username, password: hashedPassword },
-  //     process.env.SECRET,
-  //     {
-  //       expiresIn: "60s",
-  //     }
-  //   );
-  //   let sqlLastOne = "SELECT * FROM users WHERE id = ?";
-  //   connection.query(sqlLastOne, [lastInsertId], (err, result) => {
-  //     res.status(200).json(token);
-  //   });
-  // });
+  const { username, password, email } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const insertSql =
+    "INSERT INTO vuef (username, password, email) VALUES (?, ?, ?)";
+  connection.query(
+    insertSql,
+    [username, hashedPassword, email],
+    (err, result) => {
+      if (err) throw err;
+      let lastInsertId = result.insertId;
+      const token = jwt.sign(
+        { id: lastInsertId, username, password: hashedPassword, email },
+        process.env.SECRET,
+        {
+          expiresIn: "60s",
+        }
+      );
+      let sqlLastOne = "SELECT * FROM users WHERE id = ?";
+      connection.query(sqlLastOne, [lastInsertId], (err, result) => {
+        res.status(200).json(token);
+      });
+    }
+  );
 });
 
 module.exports = router;
